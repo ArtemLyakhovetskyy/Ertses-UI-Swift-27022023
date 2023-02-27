c7c8efdf40e5cdfa239b084d4ba95b3cee7db6a0
ArtemLyakhovetskyy <115452070+ArtemLyakhovetskyy@users.noreply.github.com>
2023-02-27 10:41:14 +0100

    Initial Commit

---
+++ 27022023/_7022023App.swift
@@ -0,0 +1,17 @@
+//
+//  _7022023App.swift
+//  27022023
+//
+//  Created by Artem Lyakhovetskyy on 27.02.23.
+//
+
+import SwiftUI
+
+@main
+struct _7022023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
